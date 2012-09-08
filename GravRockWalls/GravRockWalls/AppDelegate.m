f9d4700f46247de880e4e217d074d726ce9d1264
Justin Shacklette <shackletj@gmail.com>
2012-09-08 08:08:27 -0600

    correctly set to use only portrait orientation

--- GravRockWalls/GravRockWalls/AppDelegate.m
+++ GravRockWalls/GravRockWalls/AppDelegate.m
@@ -92,7 +92,7 @@
 // Supported orientations: Landscape. Customize it for your own needs
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-	return NO; //UIInterfaceOrientationIsPortrait(interfaceOrientation);
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 
